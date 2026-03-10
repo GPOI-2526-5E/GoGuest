7975a43fbd2ce86c150d49989b31b41f99a51826
unknown <i.agosto.2940@vallauri.net>
2026-03-10 13:34:15 +0100

    bandiere e componente entry-form

---
+++ client/src/app/features/auth/entry-form.component/entry-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-form.component',
+  imports: [],
+  templateUrl: './entry-form.component.html',
+  styleUrl: './entry-form.component.css',
+  standalone: true,
+})
+export class EntryFormComponent {
+
+}
