6f653ef26930ca7fbbffc508860d77e1c84805d4
Mattia Miretti <m.miretti.3190@vallauri.edu>
2026-03-13 10:23:36 +0100

    creazione sign inizio spostamento

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login.component/login.component';
 import { EntryFormComponent } from './features/auth/entry-form.component/entry-form.component';
+import { SignComponent } from './features/sign.component/sign.component';
 
 export const routes: Routes = [
     //Route di default che manda a /home
@@ -12,4 +13,7 @@ export const routes: Routes = [
     //{ path: 'entry', component: ExitFormComponent }, 
     //Route per lettore di qrcode
     //{ path: 'entry', component: QrCodeComponent }, 
+    
+    // route per la firma
+    { path: 'sign', component: SignComponent }, 
 ];
--- client/src/app/features/auth/entry-form.component/entry-form.component.ts
+++ client/src/app/features/auth/entry-form.component/entry-form.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable, timer } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router'; 
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'app-entry-form.component',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './entry-form.component.html',
   styleUrl: './entry-form.component.css',
   standalone: true,
@@ -17,4 +18,33 @@ export class EntryFormComponent {
       map(() => new Date())
     );
 
+    // 1. CREIAMO IL MODELLO DEL FORM
+  // Ogni campo corrisponde a un input nel tuo HTML
+  entryForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    company: new FormControl('', Validators.required),
+    referent: new FormControl('', Validators.required),
+    // Per la privacy, verifichiamo che sia obbligatoriamente "true" (spuntata)
+    privacyConsent: new FormControl(false, Validators.requiredTrue) 
+  });
+
+  // 2. FUNZIONE CHE SI ATTIVA AL CLICK DEL PULSANTE
+  onSubmit() {
+    // Controlliamo se tutti i campi richiesti sono stati compilati
+    if (this.entryForm.valid) {
+      // Se è valido, stampiamo i dati nella console del browser!
+      console.log('Dati pronti per il database:', this.entryForm.value);
+      
+      // Qui, in futuro, scriveremo il codice per inviare i dati al server/database
+
+      /*
+      TO DO
+      */
+
+    } else {
+      console.log('Attenzione: Il form non è compilato correttamente.');
+      // Angular evidenzierà automaticamente in rosso i campi mancanti se impostiamo il CSS
+    }
+  }
 }
